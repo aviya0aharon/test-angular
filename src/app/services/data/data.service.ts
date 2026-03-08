3c352820c9e0bd15e1ce7914705f5adfe9f4cb44
aviya0aharon <avaharon12@gmail.com>
2026-03-08 12:05:26 +0200

    Add files via upload

---
+++ src/app/services/data/data.service.ts
@@ -0,0 +1,23 @@
+// newsletter.service.ts
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+
+export type EmailResponse = {
+  email: string,
+  recivedDate: string
+}
+
+@Injectable({ providedIn: 'root' })
+export class DataService {
+  private http = inject(HttpClient);
+  private readonly API_URL = 'https://localhost:7079/';
+
+  // We return a Promise here to work easily with async/await in the component
+  subscribeEmail(email: string): Promise<EmailResponse> {
+    const route = 'email';
+    return firstValueFrom(
+      this.http.post<EmailResponse>(this.API_URL + route, { email })
+    );
+  }
+}
